167b802b9cfb1b6b2e3622befc732be39796ab61
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-15 15:14:07 +0200

    create StudentRepository

---
+++ src/main/java/de/iav/studendb_jakarta/repository/StudentRepository.java
@@ -0,0 +1,36 @@
+package de.iav.studendb_jakarta.repository;
+
+import de.iav.studendb_jakarta.model.Student;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@ApplicationScoped
+public class StudentRepository {
+
+    List<Student> studentList;
+
+    public StudentRepository() {
+        studentList = new ArrayList<>();
+    }
+
+    public List<Student> getStudentList(){
+        return studentList;
+    }
+
+    public Student getStudentById(String id){
+        for(Student student : studentList){
+            if(student.id().equals(id)){
+                return student;
+            }
+        }
+        throw new NoSuchElementException("Student with id: " + id + " does not exist!");
+    }
+
+    public Student addStudent(Student student){
+        studentList.add(student);
+        return student;
+    }
+}
